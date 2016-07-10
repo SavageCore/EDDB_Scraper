b8702756310412dd3ea2aa4e6b7a2a64d866be9b
SavageCore <talk@savagecore.eu>
2016-07-10 17:50:44 +0100

    First commit/version

---
+++ EDDB_Scraper/Class1.vb
@@ -0,0 +1,118 @@
+﻿Imports System.Linq
+Imports System.Text.RegularExpressions
+Imports HtmlAgilityPack
+
+Public Class EDDB_Scraper
+
+    Public Shared Function VA_DisplayName() As String
+        Return "EDDB Loop Scraper"
+    End Function
+
+    Public Shared Function VA_Id() As Guid
+        Return New Guid("{CA49A0E3-8ECA-45CC-B8EF-B309E9D8A953}")
+    End Function
+
+    Public Shared Function VA_DisplayInfo() As String
+        Return String.Empty
+    End Function
+
+    Public Shared Sub VA_Init1(ByRef state As Dictionary(Of String, Object), ByRef smallIntValues As Dictionary(Of String, Int16?), ByRef textValues As Dictionary(Of String, String), ByRef intValues As Dictionary(Of String, Integer?), ByRef decimalValues As Dictionary(Of String, Decimal?), ByRef booleanValues As Dictionary(Of String, Boolean?), ByRef extendedValues As Dictionary(Of String, Object))
+        textValues.Add("EDLP_Version", "0.1.0")
+        textValues.Add("EDLP_Initialised", "EDDB Loop Scrape " + textValues("EDLP_Version") + " Loaded")
+    End Sub
+
+    Public Shared Sub VA_Exit1(ByRef state As Dictionary(Of String, Object))
+
+    End Sub
+
+    Public Shared Sub VA_Invoke1(context As String, ByRef state As Dictionary(Of String, Object), ByRef smallIntValues As Dictionary(Of String, Int16?), ByRef textValues As Dictionary(Of String, String), ByRef intValues As Dictionary(Of String, Integer?), ByRef decimalValues As Dictionary(Of String, Decimal?), ByRef booleanValues As Dictionary(Of String, Boolean?), ByRef extendedValues As Dictionary(Of String, Object))
+
+        If (context.Contains("loop config")) Then
+            Dim LoopID = intValues("EDLP_LoopID")
+            Dim EDDBLoopURL = "https://eddb.io/trade/loop/"
+            Dim url = EDDBLoopURL + LoopID.ToString
+            Dim Web = New HtmlWeb()
+            Dim Doc = Web.Load(url)
+            Dim systemID As Integer
+            Dim stationName As String
+            Dim postitionFromStar As Integer
+
+            ' Get System1 Name
+            For Each node As HtmlNode In Doc.DocumentNode.SelectNodes("//div[contains(@class,'loop-station-left')]/a[contains(@href,'system')]")
+                textValues.Add("EDLP_System1", node.ChildNodes(0).InnerHtml)
+                ' Get ID for System1
+                systemID = getSystemID(node.Attributes("href").Value)
+                textValues.Add("EDLP_System1_id", getSystemID(systemID))
+            Next
+            ' Get Station1 Name
+            For Each node As HtmlNode In Doc.DocumentNode.SelectNodes("//div[contains(@class,'loop-station-left')]/a[contains(@href,'station')]")
+                stationName = node.ChildNodes(0).InnerHtml
+                textValues.Add("EDLP_Station1", stationName)
+                ' Get position from star
+                postitionFromStar = getStationListByDistance(systemID, stationName)
+                intValues.Add("EDLP_Station1Distance", postitionFromStar)
+            Next
+            ' Get System2 Name
+            For Each node As HtmlNode In Doc.DocumentNode.SelectNodes("//div[contains(@class,'loop-station-right')]/a[contains(@href,'system')]")
+                textValues.Add("EDLP_System2", node.ChildNodes(0).InnerHtml)
+                ' Get ID for System2
+                systemID = getSystemID(node.Attributes("href").Value)
+                textValues.Add("EDLP_System2_id", getSystemID(systemID))
+            Next
+            ' Get Station2 Name
+            For Each node As HtmlNode In Doc.DocumentNode.SelectNodes("//div[contains(@class,'loop-station-right')]/a[contains(@href,'station')]")
+                stationName = node.ChildNodes(0).InnerHtml
+                textValues.Add("EDLP_Station2", stationName)
+                ' Get position from star
+                intValues.Add("EDLP_Station2Distance", getStationListByDistance(systemID, stationName))
+            Next
+            ' Get Buy1 Name
+            For Each node As HtmlNode In Doc.DocumentNode.SelectNodes("(//div[contains(@class,'loop-actions')]//a[contains(@href,'commodity')])[1]")
+                textValues.Add("EDLP_Buy1", node.ChildNodes(0).InnerHtml)
+            Next
+            ' Get Buy1 Name
+            For Each node As HtmlNode In Doc.DocumentNode.SelectNodes("(//div[contains(@class,'loop-actions')]//a[contains(@href,'commodity')])[2]")
+                textValues.Add("EDLP_Buy2", node.ChildNodes(0).InnerHtml)
+            Next
+        End If
+
+    End Sub
+
+    Public Shared Function getSystemID(href As String) As Integer
+        Dim expression As New Regex("\d+")
+        Dim results = expression.Matches(href)
+        For Each match As Match In results
+            Return match.Value
+        Next
+        Return False
+    End Function
+
+    Public Shared Function getStationListByDistance(systemID As Integer, stationName As String)
+        Dim EDDBSystemURL As String = "https://eddb.io/system/"
+        Dim StationURL As String = EDDBSystemURL & Convert.ToString(systemID)
+        Dim Web = New HtmlWeb()
+        Dim Doc = Web.Load(StationURL)
+        Dim dictionary = New Dictionary(Of String, Integer)()
+
+        For Each stationNode As HtmlNode In Doc.DocumentNode.SelectNodes("//tr[count(preceding-sibling::tr[@class=""stationTypeGroup""])=1]//td")
+            Dim expression As New Regex("([a-zA-Z']+ [a-zA-Z']+)\s*([ML])\s*([0-9,]+) ls")
+            Dim results = expression.Matches(stationNode.InnerText)
+
+            For Each match As Match In results
+                dictionary.Add(match.Groups(1).Value, Int32.Parse(match.Groups(3).Value))
+            Next
+        Next
+
+        Dim counter As Integer = 0
+        For Each station As KeyValuePair(Of String, Integer) In dictionary.OrderBy(Function(key) key.Value)
+            counter += 1
+            If station.Key.Contains(stationName) Then
+                Return counter
+            End If
+        Next
+
+        Return False
+
+    End Function
+
+End Class
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # EDDB_Scraper
-Voice Attack Plugin to scrape eddb.io
+Voice Attack Plugin to scrape [eddb.io](https://eddb.io/) for information regarding a specific two station loop.
+
+Requires [EDDI](https://github.com/cmdrmcdonald/EliteDangerousDataProvider), [Html Agility Pack](https://htmlagilitypack.codeplex.com/)
+
+The included VoiceAttack profile should offer the full functionality and import just fine into exisitng profiles.
+
+## Installation
+##### Method 1
+1. Download and extract [Html Agility Pack](https://htmlagilitypack.codeplex.com/)
+2. Clone or download repo
+3. Compile with [Visual Studio Community](https://www.visualstudio.com/en-us/products/visual-studio-community-vs.aspx) (free)
+
+    a. Make sure reference to Html Agility Pack points to /Net40/HtmlAgilityPack.dll
+
+4. Copy `EDDB_Scraper.dll` and `HtmlAgilityPack.dll` to `C:\Program Files (x86)\VoiceAttack\Apps\EDDB_Scraper`
+5. Import `EDDB_Scraper-Profile.vap`
+
+    a. Keybinding commands are identical to [HCS](http://www.hcsvoicepacks.com/) profiles so skip import of those if you use it
+
+##### Method 2
+1. Download compiled plugin from [Releases](https://github.com/SavageCore/EDDB_Scraper/releases) page
+2. Extract and copy `EDDB_Scraper.dll` and `HtmlAgilityPack.dll` to `C:\Program Files (x86)\VoiceAttack\Apps\EDDB_Scraper`
+3. Import `EDDB_Scraper-Profile.vap`
+
+    a. Keybinding commands are identical to [HCS](http://www.hcsvoicepacks.com/) profiles so skip import those if you use it
+
+## How to use
+
+1. Edit command `((EDDB_Scraper - Config))`
+2. Set Integer `EDLP_LoopID` to numerical value from [`https://eddb.io/trade/loop/1481127`](https://eddb.io/trade/loop/1481127)
+3. Optionally enable Debug log printing by setting Boolean `EDLP_Debug` to True
+4. Say `Plot next trade route`
+5. Done - VA should open Galaxy map, search for System, open System Map and finally plot route to station
+
+If you forget which item you should buy at current station just say `What should I buy next`
